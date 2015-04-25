9d7c6f380a6298694921294d3324bfcc37844d8d
Isaac Ojeda <isaacoq@gmail.com>
2015-04-25 14:20:03 -0600

    Example 1 completed
    now you can change de template with javascript and see how the same
    data source works with differents templates

--- Post 1/app.js
+++ Post 1/app.js
@@ -1,70 +1,45 @@
     $(function(){
-      
+
       //http://jsbin.com/kedizeraze/1/
-      
+
       var brandImage = "http://cdn.cyberpuerta.mx/out/pictures/emthumbnailer/160/20/bd2/c7e/5dd/aoclogo-ea4c0ba4.jpg";
       var productImage= "http://cdn.cyberpuerta.mx/out/pictures/emthumbnailer/200/150/bb3/6a7/ebe/21994713-762-ebdf0b62.jpg";
       var products = [];
-      
-      products.push({
-        brandImage:brandImage ,        
-        productImage:productImage,
-        description: "AOC Monitor E1670SWU LED 15.6'', Widescreen, Negro",
-        price: 1186.00,
-        stock: 100
-      });
-      
-      products.push({
-        brandImage:brandImage ,        
-        productImage:productImage,
-        description: "AOC Monitor E1670SWU LED 15.6'', Widescreen, Negro",
-        price: 1186.00,
-        stock: 100
-      });
-      
-      products.push({
-        brandImage:brandImage ,        
-        productImage:productImage,
-        description: "AOC Monitor E1670SWU LED 15.6'', Widescreen, Negro",
-        price: 1186.00,
-        stock: 100
-      });
-      
-      products.push({
-        brandImage:brandImage ,        
-        productImage:productImage,
-        description: "AOC Monitor E1670SWU LED 15.6'', Widescreen, Negro",
-        price: 1186.00,
-        stock: 100
-      });
-      
-      products.push({
-        brandImage:brandImage ,        
-        productImage:productImage,
-        description: "AOC Monitor E1670SWU LED 15.6'', Widescreen, Negro",
-        price: 1186.00,
-        stock: 100
-      });
-      
-      products.push({
-        brandImage:brandImage ,        
-        productImage:productImage,
-        description: "AOC Monitor E1670SWU LED 15.6'', Widescreen, Negro",
-        price: 1186.00,
-        stock: 100
-      });
-      
-      products.push({
-        brandImage:brandImage ,        
-        productImage:productImage,
-        description: "AOC Monitor E1670SWU LED 15.6'', Widescreen, Negro",
-        price: 1186.00,
-        stock: 100
-      });      
-      
+
+
+      for (var i = 1; i < 20; i++) {
+        products.push({
+          brandImage:brandImage ,
+          productImage:productImage,
+          description: i + "- AOC Monitor E1670SWU LED 15.6'', Widescreen, Black",
+          price: 1186.00,
+          stock: 100,
+          descriptions:[
+            "Screen size: 15.6\"",
+            "Screen resolution: 1920 x 1080",
+            "Ratio: 16:9"
+          ]
+        });
+      }
+
+
       var viewModel = kendo.observable({
-        products : products        
+        products : products
       });
-      
+
       kendo.bind($("#productsView"),viewModel);
-    }); 
+
+
+      $("#btnList,#btnGrid").on("click",function(){
+
+        var templateName = $(this).data("template");
+        var kendoTemplate = kendo.template($("#" + templateName).html());
+
+
+        var listView = $("#products").data("kendoListView");
+        
+        listView.template = kendoTemplate;
+        listView.altTemplate = kendoTemplate;
+        listView.refresh();
+      });
+    });
